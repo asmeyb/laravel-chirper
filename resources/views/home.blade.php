8fdc8953c5d2296b2f617bd9e24457a9f276d902
asmeyb <asmeyb@gmail.com>
2025-09-19 15:43:47 +0300

    our first change after

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,11 @@
     <x-slot:title>Welcome</x-slot:title>
     <div class="card bg-base-100 shadow max-w-xl mx-auto">
         <div class="card-body">
-            <h1 class="text-3xl font-bold">Welcome to Chirper</h1>
-            <p class="mt-4 text-base-content/60">This is a simple Laravel application.</p>
+            <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+            <p class="mt-4 text-base-content/60">This is a simple Laravel application. Time to make it sing (or Chirp)!</p>
+            <p class="mt-2 text-sm text-gray-600">
+                Now live on the internet! 🎉
+            </p>
         </div>
     </div>
 </x-layout>
