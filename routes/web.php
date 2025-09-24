d153dd898e33752a86fd4cc04bca23377845e77c
asmeyb <asmeyb@gmail.com>
2025-09-24 11:34:33 +0300

    Adding some CSS features and fix some bugs on Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChirpController;
-use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\ChirpController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index']);
 
+// Protected routes
 Route::middleware('auth')->group(function () {
     Route::post('/chirps', [ChirpController::class, 'store']);
     Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
@@ -15,19 +16,17 @@ Route::middleware('auth')->group(function () {
     Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
 
+// Registration routes
 Route::view('/register', 'auth.register')
     ->middleware('guest')
     ->name('register');
- 
 Route::post('/register', Register::class)
     ->middleware('guest');
-
 
 // Login routes
 Route::view('/login', 'auth.login')
     ->middleware('guest')
     ->name('login');
-
 Route::post('/login', Login::class)
     ->middleware('guest');
 
